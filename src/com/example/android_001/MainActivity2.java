42a59c6da7beb559c71cb5a78cb7919c5f5d6f7e
yesen <1132714697@qq.com>
2014-11-15 19:47:05 +0800

    增加一个Activity

---
+++ src/com/example/android_001/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.android_001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity2 extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+    	super.onCreate(savedInstanceState);
+    }
+}
